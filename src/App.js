432dd693749664d1c81bd349e0f56b73016d4e7c
aly_dev07 <aly_dev07@yahoo.com>
2023-09-15 17:56:59 +1200

    First commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { Canvas } from "@react-three/fiber"
+import { Sky, PointerLockControls, KeyboardControls } from "@react-three/drei"
+import { Physics, RigidBody } from "@react-three/rapier"
+import { Ground } from "./Ground"
+import { Player } from "./Player"
+
+import { Model as Building } from "./Building";
+
+export default function App() {
+  return (
+    <KeyboardControls
+      map={[
+        { name: "forward", keys: ["ArrowUp", "w", "W"] },
+        { name: "backward", keys: ["ArrowDown", "s", "S"] },
+        { name: "left", keys: ["ArrowLeft", "a", "A"] },
+        { name: "right", keys: ["ArrowRight", "d", "D"] },
+        { name: "jump", keys: ["Space"] },
+      ]}>
+      <Canvas shadows camera={{ fov: 45 }}>
+        <Sky sunPosition={[100, 20, 100]} />
+        <ambientLight intensity={0.3} />
+        <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+        <Physics gravity={[0,-30,0]} >
+          <Ground />
+
+          <RigidBody type="fixed" colliders="trimesh">
+            <Building scale={0.0175} position={[0,0.001,-10]}/>
+          </RigidBody>
+          
+          <Player />
+
+        </Physics>
+        <PointerLockControls />
+      </Canvas>
+    </KeyboardControls>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client"
+import "./styles.css"
+import App from "./App"
+
+function Overlay() {
+  return (
+    <>
+      <App />
+      <div className="dot" />
+    </>
+  )
+}
+
+createRoot(document.getElementById("root")).render(<Overlay />)
